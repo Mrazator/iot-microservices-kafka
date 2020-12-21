451acffb99f53351bba4540fe67f8603d1436948
Michal Randak <michal.randak@gmail.com>
2020-12-21 12:58:17 +0100

    added archiver to build and docker-compose files

--- buildImages.bat
+++ buildImages.bat
@@ -1,4 +1,13 @@
 REM build data generator image
 cd ./services/data-generator
+call mvn clean install -DskipTests
 call docker build -f src/main/docker/Dockerfile.jvm -t "data-generator:latest"  .
 cd ../..
+echo "Generator Image Built!"
+
+REM build data archiver image
+cd ./services/data-archiver
+call mvn clean install -DskipTests
+call docker build -f src/main/docker/Dockerfile.jvm -t "data-archiver:latest"  .
+cd ../..
+echo "Archiver Image Built!"
