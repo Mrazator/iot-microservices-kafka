3bffaa85c8572c4357c1a005ee4cd8e87c734d87
Michal Randak <michal.randak@gmail.com>
2021-01-04 19:29:15 +0100

    added debug log in collector consuming data

--- services/data-collector/src/main/java/com/nabiyev/pv217/iotservices/datacollector/kafka/KafkaEndpoint.java
+++ services/data-collector/src/main/java/com/nabiyev/pv217/iotservices/datacollector/kafka/KafkaEndpoint.java
@@ -3,16 +3,20 @@ package com.nabiyev.pv217.iotservices.datacollector.kafka;
 import com.nabiyev.pv217.iotservices.datacollector.data.Report;
 import com.nabiyev.pv217.iotservices.datacollector.data.ReportCollect;
 import com.nabiyev.pv217.iotservices.datacollector.services.CollectorService;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @ApplicationScoped
 public class KafkaEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaEndpoint.class);
 
     @Inject
     public CollectorService service;
@@ -20,6 +24,7 @@ public class KafkaEndpoint {
     @Incoming("measurements-generated-in")
     @Outgoing("measurements-collected-out")
     public ReportCollect processReport(String reportJson) {
+        LOGGER.debug("incoming kafka message "+reportJson);
         Jsonb jsonb = JsonbBuilder.create();
         Report input = jsonb.fromJson(reportJson, Report.class);
 
