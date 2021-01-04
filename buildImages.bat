7ddc5ce3730f98a0f9b4890be5e75e13d12322a9
Michal Randak <michal.randak@gmail.com>
2021-01-04 19:26:49 +0100

    synced bat with sh script

--- buildImages.bat
+++ buildImages.bat
@@ -5,6 +5,14 @@ call docker build -f src/main/docker/Dockerfile.jvm -t "data-generator:latest"  
 cd ../..
 echo "Generator Image Built!"
 
+REM build data collector image
+cd ./services/data-collector
+call mvn clean install -DskipTests 
+call docker build -f src/main/docker/Dockerfile.jvm -t "data-collector:latest" .
+cd ../..
+echo "Collector Image Built!"
+
+
 REM build data archiver image
 cd ./services/data-archiver
 call mvn clean install -DskipTests 
